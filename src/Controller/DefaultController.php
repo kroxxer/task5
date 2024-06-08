d25ceb33aaf53338e075d4108778e90a7d39d5b1
Aboba <valikcrazy@gmail.com>
2024-06-08 21:21:45 +0300

    a

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,33 +6,58 @@ use App\Entity\Region;
 use App\Service\PersonFactory;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class DefaultController extends AbstractController
 {
 
     private const PERSONS_CREATE_AMOUNT = 20;
     private const PERSONS_UPDATE_AMOUNT = 10;
-    #[Route('/', name: 'app_index')]
-    public function index(Request $request,PersonFactory $personFactory): Response
+
+    private const SEED_DEFAULT = 1234;
+
+    private const ERRORS_AMOUNT_DEFAULT = 0;
+
+    public function __construct(private PersonFactory $personFactory = new PersonFactory())
     {
-        dump($request);
-        $persons = $personFactory->createMany(self::PERSONS_CREATE_AMOUNT, 1234, 0, Region::Poland->value);
-        return $this->render('default/index.html.twig', [
-            'persons' => $persons,
-            'regions' => Region::cases()
-        ]);
     }
 
-    #[Route('/update', name: 'app_update')]
-    public function update(PersonFactory $personFactory): Response
+    #[Route('/', name: 'app_index', methods: ['GET'])]
+    public function index(PersonFactory $personFactory): Response
     {
-        $persons = $personFactory->updateMany(self::PERSONS_UPDATE_AMOUNT);
-
+        $persons = $this->personFactory->createMany(self::PERSONS_UPDATE_AMOUNT);
         return $this->render('default/index.html.twig', [
+            'regions' => Region::cases(),
+            'persons' => $persons
+        ]);
+    }
+    #[Route('/create', name: 'app_create', methods: ['POST'])]
+    public function create(Request $request, SerializerInterface $serializer): Response
+    {
+        $postData = json_decode($request->getContent());
+        $parameters = array();
+        foreach ($postData as $data)
+            $parameters[$data[0]] = $data[1];
+        $persons = $this->personFactory->createMany(self::PERSONS_CREATE_AMOUNT, $parameters['seed'],  $parameters['error'],  $parameters['region']);
+        $response = $this->renderView('default/persons.html.twig', [
+            'persons' => $persons
+        ]);
+
+        return new Response($response);
+    }
+
+    #[Route('/update', name: 'app_update', methods: ['POST'])]
+    public function update(Request $request): Response
+    {
+        $persons = $this->personFactory->updateMany(self::PERSONS_UPDATE_AMOUNT);
+
+        $response = $this->renderView('default/persons.html.twig', [
             'persons' => $persons,
         ]);
+        return new Response($response);
     }
 }
